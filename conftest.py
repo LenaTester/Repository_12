b4bbe3be2f283cc070e9439ecfc11bcfca872dd3
LenaTester <106830652+LenaTester@users.noreply.github.com>
2022-07-26 20:03:46 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,33 +1,9 @@
 from contextlib import suppress
-import allure
 import pytest
 
 from pythonProject.Repository_3.framework_from_scratch.page_objects.home_page import HomePage
 from pythonProject.Repository_3.framework_from_scratch.utilities.driver_factory import DriverFactory
 from pythonProject.Repository_3.framework_from_scratch.utilities.read_run_settings import ReadConfig
-
-@pytest.hookimpl(hookwrapper=True, tryfirst=True)
-def pytest_runtest_makereport(item, call):
-    outcome = yield
-    rep = outcome.get_result()
-    setattr(item, "rep_" + rep.when, rep)
-    return rep
-
-@pytest.fixture()
-def create_driver(request):
-    driver = DriverFactory.create_driver(driver_id=ReadConfig.get_driver_id(), is_headless=ReadConfig.get_headless_mod())
-    driver.get('http://loveread.ec/')
-    yield driver
-    if request.node.rep_call.failed:
-        with suppress(Exception):
-            allure.attach(driver.get_screenshot_as_png(),
-                          name=request.function.__name__,
-                          attachment_type=allure.attachment_type.PNG)
-    driver.quit()
-
-@pytest.fixture()
-def get_home_page(create_driver):
-    return HomePage(create_driver)
 
 @pytest.fixture()
 def successful_login(get_home_page, create_driver):
